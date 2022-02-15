dec47db387836b4541856b49e35bc489d097c944
김규현 <kgh2703@irlink.co.kr>
2022-02-15 15:28:30 +0900

    english-word 패키지 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,6 +8,8 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+
     return MaterialApp(
       title: "Flutter App",
       home: Scaffold(
@@ -14,7 +17,7 @@ class MyApp extends StatelessWidget {
           title: Text("Flutter App"),
         ),
         body: Center(
-          child: Text("손소독"),
+          child: Text(wordPair.asPascalCase),
         ),
       ),
     );
